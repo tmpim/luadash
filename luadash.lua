55241d3e28c84309eba9ce99dd1bb91e2f57b50f
Incinirate <dogescript@gmail.com>
2018-07-15 17:05:37 -0600

    make more versatile

--- luadash.lua
+++ luadash.lua
@@ -1,5 +1,5 @@
 local doc_path = '/doc/'
-local topic = ((...) or 'index') .. '.txt'
+local topic = ((...) or 'index')
 
 -- Levenshtein Distance
 local function distance(str1, str2)
@@ -35,8 +35,8 @@ local function distance(str1, str2)
  return v0[#str2]
 end
 
-if fs.exists(doc_path .. topic) then
-  local f = fs.open(doc_path .. topic, 'r')
+if fs.exists(doc_path .. topic .. ".txt") then
+  local f = fs.open(doc_path .. topic .. ".txt", 'r')
   local s = f.readAll()
   local lns = select(2, s:gsub('\n', '\n'))
   if lns > 15 then
@@ -61,20 +61,29 @@ if fs.exists(doc_path .. topic) then
 else
   printError('No documentation for ' .. topic)
   local list = fs.list(doc_path)
-  print 'Did you mean one of:'
 
   local candidates = {}
   for i = 1, #list do
+    local item = list[i]:match('^(.+)%.txt$') or list[i]
+    local mindist = math.huge
+    for j = 1, math.max(#item - #topic, 1) do
+      local dist = distance(topic, item:sub(j, j + #topic)) + j - 1
+      if dist < mindist then
+        mindist = dist
+      end
+    end
 
-    local dist = distance(list[i], topic)
-    if dist < (#topic - 4) then
-      candidates[#candidates + 1] = {dist, list[i]}
+    if mindist < 4 then
+      candidates[#candidates + 1] = {mindist, item}
     end
   end
 
-  table.sort(candidates, function(a, b) return a[1] < a[1] end)
+  table.sort(candidates, function(a, b) return a[1] < b[1] end)
 
-  for i = 1, #candidates do
-    print(' \183 ' .. candidates[i][2])
+  if #candidates > 0 then
+    print 'Did you mean:'
+    for i = 1, #candidates do
+      print(' \183 ' .. candidates[i][2])
+    end
   end
 end
