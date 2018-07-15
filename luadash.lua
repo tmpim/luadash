c6e51554f6711d63458ef505966b9d6d5c7a39fd
Incinirate <dogescript@gmail.com>
2018-07-15 16:24:21 -0600

    change search heuristic to levenshtein

--- luadash.lua
+++ luadash.lua
@@ -1,16 +1,38 @@
 local doc_path = '/doc/'
 local topic = ((...) or 'index') .. '.txt'
 
--- terrible heuristic haha it'll catch typos
-local function distance(s, t)
-  local dist, s, t = 0, s:sub(1, -5), t:sub(1, -5)
-  for i = 1, #t do
-    if s:sub(i, i) ~= t:sub(i, i) then
-      dist = dist + (#t - i)
-    end
-  end
-  local sd = #s > #t and #s - #t or #t - #s
-  return math.abs(dist + sd)
+-- Levenshtein Distance
+local function distance(str1, str2)
+  local v0 = {}
+ local v1 = {}
+
+ for i = 0, #str2 do
+   v0[i] = i
+ end
+
+ for i = 0, #str1 - 1 do
+   v1[0] = i + 1
+
+   for j = 0, #str2 - 1 do
+     local delCost = v0[j + 1] + 1
+     local insertCost = v1[j] + 1
+     local subCost
+
+     if str1:sub(i + 1, i + 1) == str2:sub(j + 1, j + 1) then
+       subCost = v0[j]
+     else
+       subCost = v0[j] + 1
+     end
+
+     v1[j + 1] = math.min(delCost, insertCost, subCost)
+   end
+
+   local t = v0
+   v0 = v1
+   v1 = t
+ end
+
+ return v0[#str2]
 end
 
 if fs.exists(doc_path .. topic) then
@@ -40,9 +62,19 @@ else
   printError('No documentation for ' .. topic)
   local list = fs.list(doc_path)
   print 'Did you mean one of:'
+
+  local candidates = {}
   for i = 1, #list do
-    if distance(list[i], topic) < (#topic - 4) then
-      print(' \183 ' .. list[i])
+
+    local dist = distance(list[i], topic)
+    if dist < (#topic - 4) then
+      candidates[#candidates + 1] = {dist, list[i]}
     end
+  end
+
+  table.sort(candidates, function(a, b) return a[1] < a[1] end)
+
+  for i = 1, #candidates do
+    print(' \183 ' .. candidates[i][2])
   end
 end
